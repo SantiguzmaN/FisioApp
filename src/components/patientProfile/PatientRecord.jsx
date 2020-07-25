ad3ae1f27a7cf52116ee807573861112eb09da08
john <john.1701421171@ucaldas.edu.co>
2020-07-24 19:14:00 -0500

    Patient profile dashboard

--- src/components/patientProfile/PatientRecord.jsx
+++ src/components/patientProfile/PatientRecord.jsx
@@ -5,12 +5,8 @@ const PatientRecord = () => {
     return alert("entra");
   };
   const citas = ["cita uno", "cita dos", "cita tres"];
-  let style = {
-    margin: "50px",
-    border: "ridge",
-  };
   return (
-    <div style={style}>
+    <div className="">
       <h5>Historial</h5>
       {citas.map((item) => (
         <p className="border" id="clic" onClick={onClick}>
