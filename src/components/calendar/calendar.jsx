a67034431d04fef6decde1b3e2dad9feca252cd8
kairox07 <samdtuxx1@gmail.com>
2020-08-02 22:22:50 -0500

    Correccion Calendar

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Calendar,momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../../styles/calendar.css';
 
 require('moment/locale/es.js');  
 const localizer = momentLocalizer(moment);
@@ -9,22 +10,20 @@ const localizer = momentLocalizer(moment);
 const calendar = () => {
   //  ToDo . this event must be received as param
   const myEvents= [{
-    title: "Actividad Fisica",
+    title: 'actividad Fisica',
     start: new Date('2020-07-05 10:22:00'),
     end: new Date('2020-07-05 10:42:00')
   }];
   
   return (
-    <div>
-      <div style={{ height: '500pt'}}>
-        <Calendar
-          events={myEvents}
-          startAccessor="start"
-          endAccessor="end"
-          defaultDate={moment().toDate()}
-          localizer={localizer}
-        />
-      </div>
+    <div className="principal-c-calendar">
+      <Calendar
+        events={myEvents}
+        startAccessor="start"
+        endAccessor="end"
+        defaultDate={moment().toDate()}
+        localizer={localizer}
+      />
     </div>
   );
 };
--- src/components/home/homeContainer.jsx
+++ src/components/home/homeContainer.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { logOut } from '../../actions/loginActions';
 import { useUserState } from '../../store/userProvider';
-import  Calendar   from "../calendar/calendar";
-import PatientProfile from '../patientProfile/patientProfile';
+import  Calendar   from '../calendar/calendar';
+//import PatientProfile from '../patientProfile/patientProfile';
 import '../../styles/home.css';
 
 const Home = () => {
