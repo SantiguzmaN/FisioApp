c01da8f298f68d790969c8e0da80c3105266ba51
john <john.1701421171@ucaldas.edu.co>
2020-08-04 00:54:02 -0500

    DoItImprovement

--- src/components/home/homeContainer.jsx
+++ src/components/home/homeContainer.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { logOut } from '../../actions/loginActions';
-import { useUserState } from '../../store/userProvider';
-import  Calendar   from '../calendar/calendar';
-//import PatientProfile from '../patientProfile/patientProfile';
-import '../../styles/home.css';
+import React, { useEffect, useState } from "react";
+import { logOut } from "../../actions/loginActions";
+import { useUserState } from "../../store/userProvider";
+import Calendar from "../calendar/calendar";
+import PatientProfile from "../patientProfile/patientProfile";
+import "../../styles/home.css";
 
 const Home = () => {
   const { user } = useUserState();
-  const [userName, setUserName,] = useState('USER NAME');
+  const [userName, setUserName] = useState("USER NAME");
 
   useEffect(() => {
     if (user) {
@@ -19,27 +19,42 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <div className="row home-container w-100">
-        <div className="border col-3 principal-Container">
-          <h4>
-            {userName}
-          </h4>
-          <h4> NOMBRE DEL CONSULTORIO </h4>
-          <div className="border w-100">
-            {/* here must go the calendar */}
-          </div>
-          <button type="button" onClick={() => logOutFetch()}>
+    <div className="wrapper App">
+      <div className="row w-100 principal-Container">
+        <div className="border col-3" id="childenConteiner">
+          <h4 id="usercolor">{userName}</h4>
+          <img
+            className="logo"
+            src={`${process.env.PUBLIC_URL}/tata.jpg`}
+            alt="FISIOAPP"
+          />
+          <div className="border w-100">{/* here must go the calendar */}</div>
+          <button
+            type="button"
+            className="btn btn-info btn-block my-4"
+            onClick={() => logOutFetch()}
+          >
             Gestionar Cita
           </button>
-          <button type="button" onClick={() => logOutFetch()}>
+          <button
+            type="button"
+            className="btn btn-info btn-block my-4"
+            onClick={() => logOutFetch()}
+          >
             Gestionar Paciente
           </button>
-          <button type="button" onClick={() => logOutFetch()}>
+          <button
+            type="button"
+            className="btn btn-info btn-block my-4"
+            onClick={() => logOutFetch()}
+          >
             Cerrar Sesion
           </button>
         </div>
-        <div id = 'root' className="border col-9 principal-Container">
+        <div
+          id="childenConteiner2"
+          className="border col-9 principal-Container"
+        >
           <Calendar />
           {/*<PatientProfile />*/}
         </div>
--- src/components/shared/background.jsx
+++ src/components/shared/background.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import ParallaxMousemove from 'react-parallax-mousemove';
-import '../../styles/background.css';
+import React from "react";
+import ParallaxMousemove from "react-parallax-mousemove";
+import "../../styles/background.css";
 
 const Background = ({ children }) => {
   const style = {
     outter: {
-      background: 'radial-gradient(50% 150%, #6CD7E8 50%, #59C2D3 100%)',
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      width: '100%',
+      background: "radial-gradient(50% 150%, #6CD7E8 50%, #59C2D3 100%)",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      width: "100%",
       height: window.innerHeight,
-      position: 'relative',
-      overflow: 'hidden'
+      position: "relative",
+      overflow: "hidden",
     },
     infoLayerStyle: {
-      position: 'relative',
-      marginTop: '10px'
+      position: "relative",
+      marginTop: "10px",
     },
     bgLayerStyle: {
-      position: 'absolute',
-      height: '100%',
-      transform: 'translate(-15%, 17%)'
+      position: "absolute",
+      height: "100%",
+      transform: "translate(-15%, 17%)",
     },
     bgLayerStyle2: {
-      position: 'absolute',
-      height: '100%',
-      transform: 'translate(170%, 17%)'
-    }
+      position: "absolute",
+      height: "100%",
+      transform: "translate(170%, 17%)",
+    },
   };
 
   return (
@@ -38,8 +38,8 @@ const Background = ({ children }) => {
           yFactor: 0.1,
           springSettings: {
             stiffness: 100,
-            damping: 30
-          }
+            damping: 30,
+          },
         }}
       >
         <img src={`${process.env.PUBLIC_URL}/bg1.png`} alt="Parallax Layer" />
@@ -51,8 +51,8 @@ const Background = ({ children }) => {
           yFactor: 0.1,
           springSettings: {
             stiffness: 100,
-            damping: 30
-          }
+            damping: 30,
+          },
         }}
       >
         <img src={`${process.env.PUBLIC_URL}/bg2.png`} alt="Parallax Layer" />
@@ -61,7 +61,7 @@ const Background = ({ children }) => {
         layerStyle={style.infoLayerStyle}
         config={{
           xFactor: 0,
-          yFactor: 0
+          yFactor: 0,
         }}
       >
         {children}
