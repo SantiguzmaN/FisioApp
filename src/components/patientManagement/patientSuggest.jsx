bc9956f2705c4e0e00ad048abe89987df169db1e
Andres Diaz <adiaz@panopen.com>
2020-08-29 00:30:28 -0500

    fix modal

--- src/components/patientManagement/patientSuggest.jsx
+++ src/components/patientManagement/patientSuggest.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../styles/patientSuggest.css';
 
-const PatientSuggest = () => {
+const PatientSuggest = (user) => {
+  console.log('usuario', user);
+  const [userName, setUserName] = useState(user.user.nombre);
+  const [userCc, setUserCc] = useState(user.cc);
 
+  console.log('eaaa', userName);
   return (
-    <div className="row patient-suggest-container">
-      <div className="col-4 suggest-name">Paciente</div>
-      <div className="col-8 row">
+    <div className="row border patient-suggest-container">
+      <div className="col-4 suggest-name">{userName}</div>
+      <div className="mb-1 col-8 row">
         <button className="col-3 btn suggest-button">PERFIL</button>
         <button className="col-3 btn suggest-button">EDITAR</button>
         <button className="col-5 btn suggest-button">AGENDAR CITA</button>
